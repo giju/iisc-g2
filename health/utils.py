f3b46e47d32fd493787211f2d5781f2642a13153
giju <giju@ceegees.in>
2024-11-14 20:44:18 +0530

    moving the eda to util functions

--- health/utils.py
+++ health/utils.py
@@ -4,6 +4,67 @@ Following are utility functions created for listing files in a directory and gen
 """
 import os
 import re
+import pandas as pd
+import numpy as np
+
+
+"""
+Column Names
+,id,
+Name,
+Gender,
+Age,
+City,
+Working Professional or Student,
+Profession,
+Academic Pressure,
+Work Pressure,
+CGPA,
+Study Satisfaction,
+Job Satisfaction,
+Sleep Duration,
+Dietary Habits,
+Degree,
+Have you ever had suicidal thoughts ?,
+Work/Study Hours,
+Financial Stress,
+Family History of Mental Illness,
+Depression
+
+"""
+
+STR_COLS = [
+    'Gender', # Cleaned
+    'City', #Cleaned
+    'Working Professional or Student', # Clean data
+    'Profession', # Minor erros need not fix as statistically numbers are not great
+    'Dietary Habits', # minor issues not statistically relevant
+    'Degree',# minor issues not statistically relevant
+    'Have you ever had suicidal thoughts ?', # Data  is clean
+    'Family History of Mental Illness', #Clean
+]
+ALLOWED_COLS = [
+    'id',
+    # 'Name',
+    'Gender', # Cleaned
+    'Age', # Cleand
+    'City', #Cleaned
+    'Working Professional or Student', # Clean data
+    'Profession', # Minor erros need not fix as statistically numbers are not great
+    'Academic Pressure', # Data is clean
+    'Work Pressure', # Data is clean
+    'CGPA', # Cleaned
+    'Study Satisfaction', #data is clean
+    'Job Satisfaction', #data is clean
+    'Sleep Duration', # Cleaned
+    'Dietary Habits', # minor issues not statistically relevant
+    'Degree',# minor issues not statistically relevant
+    'Have you ever had suicidal thoughts ?', # Data  is clean
+    'Work/Study Hours', # Clean
+    'Financial Stress', # clean
+    'Family History of Mental Illness', #Clean
+    'Depression',
+]
 
 def slugify(s):
     s = s.lower().strip()
@@ -18,3 +79,30 @@ def list_files(directory):
             absolute_paths.append(os.path.abspath(filepath))
     return absolute_paths
 # print(f)
+
+
+def load_trimaing_data() :
+    train_list = list_files('../data/processed/')
+    df_list = []
+ 
+    for f in train_list:
+        if not('train' in f):
+            continue
+
+        d = pd.read_csv(f,
+            usecols=ALLOWED_COLS,  
+            dtype={   
+            'id':int,
+            'Depression': str ,
+            'CGPA': str,
+            'Sleep Duration':str
+        })
+
+        df_list.append(d)
+            # Combine the list of dataframes
+    df =  pd.concat(df_list)
+    # df['id'] = df['id'].to_numpy()
+    return df
+
+
+
